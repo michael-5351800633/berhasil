f70bb25f3eb517e83d04f84a3e2d6ceefa9a5c25
michael <michael.535180063@stu.untar.ac.id>
2020-12-15 04:05:07 +0700

    1

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI : process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/db-untar-cafe'
+    MongoURI : process.env.MongoURI || 'mongodb://127.0.0.1:27017/db-untar-cafe'
 }
