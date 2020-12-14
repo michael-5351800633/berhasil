219e57c803c996762c5680f795205f9f98f425f3
michael <michael.535180063@stu.untar.ac.id>
2020-12-15 04:17:05 +0700

    123

--- app.js
+++ app.js
@@ -12,6 +12,55 @@ const passport = require("passport");
 
 const app = express();
 
+const {MongoClient} = require('mongodb');
+
+const uri = "mongodb+srv://michael-535180063:michael123@cluster0.zejiu.mongodb.net/cluster0?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+await client.connect();
+await listDatabases(client);
+try {
+  await client.connect();
+
+  await listDatabases(client);
+
+} catch (e) {
+  console.error(e);
+}
+finally {
+  await client.close();
+}
+
+main().catch(console.error);
+
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
+
+
 //passport config
 require("./config/passport")(passport);
 
